16acbc4a4e10ca20f791e15e3d3a64d2ab2a6854
thevickypedia <vignesh.sivanandarao@gmail.com>
2020-09-06 13:53:58 -0500

    initialize jarvis

---
+++ jarvis.py
@@ -0,0 +1,26 @@
+import pyttsx3 as audio
+import speech_recognition as sr
+
+speaker = audio.init()
+
+volume = speaker.getProperty("volume")
+print(f'Current volume is: {volume}')
+
+voices = speaker.getProperty("voices")
+speaker.setProperty("voice", voices[7].id)
+
+speaker.say("Hi, I'm Jarvis. How are you doing?")
+speaker.runAndWait()
+
+r = sr.Recognizer()
+
+with sr.Microphone() as source:
+    try:
+        print('Speak Now')
+        listener = r.listen(source)
+        recognized_text = r.recognize_google(listener)
+        print(recognized_text)
+    except sr.UnknownValueError as u:
+        print(u)
+    except sr.RequestError as e:
+        print(e)
