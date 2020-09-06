4a48f7cec072aa4b4a11516148ec6947d6ce1aa9
thevickypedia <vignesh.sivanandarao@gmail.com>
2020-09-06 13:17:34 -0500

    include exception handler

--- speech2text.py
+++ speech2text.py
@@ -3,6 +3,11 @@ import speech_recognition as sr
 r = sr.Recognizer()
 
 with sr.Microphone() as source:
-    listener = r.listen(source)
-    recognized_text = r.recognize_google(listener)
-    print(recognized_text)
+    try:
+        listener = r.listen(source)
+        recognized_text = r.recognize_google(listener)
+        print(recognized_text)
+    except sr.UnknownValueError as u:
+        print(u)
+    except sr.RequestError as e:
+        print(e)
