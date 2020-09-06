cf5134423481a83f6f854ee9b394e18f27f77f4b
thevickypedia <vignesh.sivanandarao@gmail.com>
2020-09-06 13:54:55 -0500

    include two communication

--- jarvis.py
+++ jarvis.py
@@ -24,3 +24,15 @@ with sr.Microphone() as source:
         print(u)
     except sr.RequestError as e:
         print(e)
+
+    speaker.say("How can I be of service to you?")
+    speaker.runAndWait()
+    try:
+        print('Speak Now')
+        listener = r.listen(source)
+        recognized_text = r.recognize_google(listener)
+        print(recognized_text)
+    except sr.UnknownValueError as u:
+        print(u)
+    except sr.RequestError as e:
+        print(e)
