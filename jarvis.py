0aaf24c51f02c4506f5554aed3511818e5dfe15f
thevickypedia <vignesh.sivanandarao@gmail.com>
2020-09-06 13:57:18 -0500

    speak what you heard

--- jarvis.py
+++ jarvis.py
@@ -12,25 +12,25 @@ speaker.setProperty("voice", voices[7].id)
 speaker.say("Hi, I'm Jarvis. How are you doing?")
 speaker.runAndWait()
 
-r = sr.Recognizer()
+recognizer = sr.Recognizer()
 
 with sr.Microphone() as source:
     try:
         print('Speak Now')
-        listener = r.listen(source)
-        recognized_text = r.recognize_google(listener)
+        listener = recognizer.listen(source)
+        recognized_text = recognizer.recognize_google(listener)
         print(recognized_text)
     except sr.UnknownValueError as u:
         print(u)
     except sr.RequestError as e:
         print(e)
 
-    speaker.say("How can I be of service to you?")
+    speaker.say(f"I heard {recognized_text}, How can I be of service to you?")
     speaker.runAndWait()
     try:
         print('Speak Now')
-        listener = r.listen(source)
-        recognized_text = r.recognize_google(listener)
+        listener = recognizer.listen(source)
+        recognized_text = recognizer.recognize_google(listener)
         print(recognized_text)
     except sr.UnknownValueError as u:
         print(u)
