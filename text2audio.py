89c5124e56be85972e0df93c97ccfcc2a0a1b089
thevickypedia <vignesh.sivanandarao@gmail.com>
2020-09-06 13:30:55 -0500

    change voice and print current volume

--- text2audio.py
+++ text2audio.py
@@ -1,6 +1,10 @@
 import pyttsx3 as audio
 
 speaker = audio.init()
+voices = speaker.getProperty("voices")
+speaker.setProperty("voice", voices[7].id)
+volume = speaker.getProperty("volume")
+print(f'Current volume is: {volume}')
 
 speaker.say("Hi, I'm Jarvis. How can I be of service to you?")
 speaker.runAndWait()
