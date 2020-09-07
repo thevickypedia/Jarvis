836ee44e3b6737c5bf8804d7d6bb4aeb1f191f11
thevickypedia <vignesh.sivanandarao@gmail.com>
2020-09-07 08:46:19 -0500

    more requirements updated

--- installs.sh
+++ installs.sh
@@ -3,3 +3,5 @@ python3 -m pip install pyaudio
 python3 -m pip install numpy
 python3 -m pip install pyttsx3
 python3 -m pip install pytemperature
+python3 -m pip install psutil
+python3 -m pip install shutil
