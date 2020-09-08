a9887302369956c887d52830806b767401887fcc
thevickypedia <vignesh.sivanandarao@gmail.com>
2020-09-08 09:44:32 -0500

    update installs.sh

--- installs.sh
+++ installs.sh
@@ -1,7 +1,7 @@
 brew install portaudio
 pip install --upgrade pip
 python3 -m pip install SpeechRecognition
-python3 -m pip install pyaudio
+python3 -m pip install PyAudio
 python3 -m pip install numpy
 python3 -m pip install pyttsx3
 python3 -m pip install pytemperature
