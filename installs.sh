226047db8eefd6046dfffd7c6def81385d9117e0
thevickypedia <vignesh.sivanandarao@gmail.com>
2020-09-08 07:55:13 -0500

    update installs.sh

--- installs.sh
+++ installs.sh
@@ -1,8 +1,8 @@
 brew install portaudio
+python3 -m pip install SpeechRecognition
 python3 -m pip install pyaudio
 python3 -m pip install numpy
 python3 -m pip install pyttsx3
 python3 -m pip install pytemperature
 python3 -m pip install psutil
-python3 -m pip install shutil
 python3 -m pip install wikipedia
