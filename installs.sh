5065514cdc9d4b0a64cb86df6027682ffa951d19
thevickypedia <vignesh.sivanandarao@gmail.com>
2020-09-19 18:04:34 -0500

    update installs.sh to support chat bot

--- installs.sh
+++ installs.sh
@@ -10,3 +10,6 @@ python3 -m pip install psutil
 python3 -m pip install wikipedia
 python3 -m pip install newsapi-python
 python3 -m pip install pyrh
+python3 -m pip install pytz
+python3 -m pip install ChatterBot==1.0.0
+python3 -m pip install chatterbot-corpus==1.2.0
