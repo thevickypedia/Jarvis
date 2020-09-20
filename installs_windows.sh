33a612be3635fe63044ab4deb729fee05817492c
thevickypedia <vignesh.sivanandarao@gmail.com>
2020-09-20 09:38:01 -0500

    update installs_windows.sh to support chatbot

--- installs_windows.sh
+++ installs_windows.sh
@@ -1,5 +1,6 @@
 echo "**********************************************************************************************************"
 echo "Install Anaconda and make sure 'pip3' command works before proceeding."
+echo "Refer this link for pip3 installation guide: https://stackoverflow.com/questions/41501636/how-to-install-pip3-on-windows"
 echo "**********************************************************************************************************"
 echo "Starting conda installations"
 conda install portaudio
@@ -14,3 +15,8 @@ pip3 install psutil
 pip3 install wikipedia
 pip3 install newsapi-python
 pip3 install pyrh
+pip3 install pytz
+pip3 install ChatterBot==1.0.0 --ignore-installed PyYAML
+pip3 install chatterbot-corpus==1.2.0 --ignore-installed PyYAML
+pip3 install sqlalchemy==1.2
+#pip3 install python-dateutil==2.7
