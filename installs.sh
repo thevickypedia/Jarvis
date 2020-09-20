cf384c6f0a3b0aa18b2a04a6c9af4250bbb4deaa
thevickypedia <vignesh.sivanandarao@gmail.com>
2020-09-20 18:18:20 -0500

    so much of a hacky way to install chromedriver

--- installs.sh
+++ installs.sh
@@ -13,3 +13,10 @@ python3 -m pip install pyrh
 python3 -m pip install pytz
 python3 -m pip install ChatterBot==1.0.0
 python3 -m pip install chatterbot-corpus==1.2.0
+echo "*** Checking your chrome version to install chromedriver ***"
+chrome_v=$(/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --version)
+echo $chrome_v
+raw_v=$(echo $chrome_v | grep -Eo '[0-9]+([.][0-9]+)' | tr '\n' ' '; echo "")
+big_v=${raw_v%.*}
+v=$(echo $big_v | tr ' ' '.';)
+python3 -m pip install chromedriver-py==$v.*
