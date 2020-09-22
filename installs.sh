0245426f9444529f9a418380fed60de973f6e66f
thevickypedia <vignesh.sivanandarao@gmail.com>
2020-09-21 19:34:24 -0500

    check for brew installation status before installing

--- installs.sh
+++ installs.sh
@@ -1,4 +1,10 @@
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+# looks for brew installation and installs only if brew is not found in /usr/local/bin
+check=$(which brew)
+condition="/usr/local/bin/brew"
+if [[ "$check" != "$condition" ]]; then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+fi
+#python3 -m venv venv
 brew install portaudio
 pip install --upgrade pip
 python3 -m pip install SpeechRecognition
@@ -11,8 +17,10 @@ python3 -m pip install wikipedia
 python3 -m pip install newsapi-python
 python3 -m pip install pyrh
 python3 -m pip install pytz
+python3 -m pip install selenium
 python3 -m pip install ChatterBot==1.0.0
 python3 -m pip install chatterbot-corpus==1.2.0
+# looks for google chrome version and installs the right chromedriver version
 echo "*** Checking your chrome version to install chromedriver ***"
 chrome_v=$(/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --version)
 echo $chrome_v
