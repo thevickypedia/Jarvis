950fe18e5574abdf5b5d92ff0d4dc16cdd959bef
thevickypedia <vignesh.sivanandarao@gmail.com>
2020-09-21 19:56:42 -0500

    remove chromedriver and selenium

--- installs.sh
+++ installs.sh
@@ -17,14 +17,5 @@ python3 -m pip install wikipedia
 python3 -m pip install newsapi-python
 python3 -m pip install pyrh
 python3 -m pip install pytz
-python3 -m pip install selenium
 python3 -m pip install ChatterBot==1.0.0
 python3 -m pip install chatterbot-corpus==1.2.0
-# looks for google chrome version and installs the right chromedriver version
-echo "*** Checking your chrome version to install chromedriver ***"
-chrome_v=$(/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --version)
-echo $chrome_v
-raw_v=$(echo $chrome_v | grep -Eo '[0-9]+([.][0-9]+)' | tr '\n' ' '; echo "")
-big_v=${raw_v%.*}
-v=$(echo $big_v | tr ' ' '.';)
-python3 -m pip install chromedriver-py==$v.*
