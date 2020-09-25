432451084b7b78941d0ad089fffee4a5b63318e4
thevickypedia <vignesh.sivanandarao@gmail.com>
2020-09-25 17:12:20 -0500

    update installs.sh to support locating iPhone

--- installs.sh
+++ installs.sh
@@ -3,6 +3,8 @@ check=$(which brew)
 condition="/usr/local/bin/brew"
 if [[ "$check" != "$condition" ]]; then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+  echo "Installing Homebrew"
+  else echo "Found Homebrew, skipping installation"
 fi
 #python3 -m venv venv
 brew install portaudio
@@ -17,5 +19,7 @@ python3 -m pip install wikipedia
 python3 -m pip install newsapi-python
 python3 -m pip install pyrh
 python3 -m pip install pytz
+python3 -m pip install pyicloud
+python3 -m pip install geopy
 python3 -m pip install ChatterBot==1.0.0
 python3 -m pip install chatterbot-corpus==1.2.0
