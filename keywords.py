96cc609597e4090c4473802172c0f33f31053213
thevickypedia <vignesh.sivanandarao@gmail.com>
2020-09-26 11:59:12 -0500

    hold keywords in a dedicated file for easy modifications

---
+++ keywords.py
@@ -0,0 +1,60 @@
+class Keywords:
+    def date(self):
+        key = ["today's date", 'current date']
+        return key
+
+    def time(self):
+        key = ["current time"]
+        return key
+
+    def weather(self):
+        key = ['weather', 'temperature']
+        return key
+
+    def system_info(self):
+        key = ['system', 'configuration']
+        return key
+
+    def webpage(self):
+        key = ['website', 'webpage', '.com']
+        return key
+
+    def wikipedia(self):
+        key = ['wikipedia', 'fact', 'facts', 'info', 'information']
+        return key
+
+    def news(self):
+        key = ['news', 'latest']
+        return key
+
+    def report(self):
+        key = ['report', 'good morning']
+        return key
+
+    def robinhood(self):
+        key = ['robinhood', 'investment', 'portfolio', 'summary']
+        return key
+
+    def apps(self):
+        key = ['launch', 'app', 'apps', 'application']
+        return key
+
+    def repeat(self):
+        key = ['repeat', 'train']
+        return key
+
+    def chatbot(self):
+        key = ['chat', 'bot']
+        return key
+
+    def location(self):
+        key = ['location', 'where am i']
+        return key
+
+    def locate(self):
+        key = ['locate', 'where is my phone']
+        return key
+
+    def exit(self):
+        key = ['exit', 'quit', 'go to sleep']
+        return key
