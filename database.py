5e1070a8e3f00d416ae24d51dc9ab2c1c351c7ff
thevickypedia <vignesh.sivanandarao@gmail.com>
2020-09-29 16:51:29 -0500

    add uploader and downloader::test data from db

--- database.py
+++ database.py
@@ -14,3 +14,24 @@ class Database:
             connection.execute(f"CREATE TABLE {table_name} (category, item)")
             connection.commit()
             return f"Database {file_name} has been created."
+
+    def uploader(self, category, item):
+        connection = sqlite3.connect(file_name)
+        connection.execute(f"INSERT INTO {table_name} (category, item) VALUES ('{category}','{item}')")
+        connection.commit()
+        return f"Table {table_name} has been updated with Category: {category} and Item: {item}."
+
+    def downloader(self):
+        connection = sqlite3.connect(file_name)
+        connector = connection.cursor()
+        connector.execute(f"SELECT category, item from {table_name}")
+        response = connector.fetchall()
+        connector.close()
+        return response
+
+
+if __name__ == '__main__':
+    data = Database().downloader()
+    for row in data:
+        result = {row[i]: row[i + 1] for i in range(0, len(row), 2)}
+        print(result)
