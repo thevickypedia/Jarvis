602f3af08a12ab94329359008c9db07a8176908f
thevickypedia <vignesh.sivanandarao@gmail.com>
2020-09-30 00:25:33 -0500

    update database.py to be asynchronous

--- database.py
+++ database.py
@@ -1,25 +1,31 @@
 import os
 import sqlite3
 
-file_name = 'todo.db'
+file_name = 'tasks.db'
 table_name = file_name.replace('.db', '')
 
 
 class Database:
     def create_db(self):
         if os.path.isfile(file_name):
-            return f"Database {file_name} already exists."
+            return f"A database named, {file_name}, already exists."
         else:
             connection = sqlite3.connect(file_name)
-            connection.execute(f"CREATE TABLE {table_name} (category, item)")
+            connection.execute(f"CREATE TABLE {table_name} (category, item, id INTEGER PRIMARY KEY )")
             connection.commit()
-            return f"Database {file_name} has been created."
+            return f"A database, {file_name}, has been created."
 
     def uploader(self, category, item):
         connection = sqlite3.connect(file_name)
+        connector = connection.cursor()
+        connector.execute(f"SELECT category, item from {table_name}")
+        response = connector.fetchall()
+        for c, i in response:
+            if i == item and c == category:
+                return f"Looks like the table: {table_name}, already has the item: {item} in, {category} category"
         connection.execute(f"INSERT INTO {table_name} (category, item) VALUES ('{category}','{item}')")
         connection.commit()
-        return f"Table {table_name} has been updated with Category: {category} and Item: {item}."
+        return f"I've updated the table: {table_name} with category: {category} and item: {item}."
 
     def downloader(self):
         connection = sqlite3.connect(file_name)
@@ -29,11 +35,17 @@ class Database:
         connector.close()
         return response
 
-    def deleter(self, category, item):
+    def deleter(self, item):
         connection = sqlite3.connect(file_name)
-        connection.execute(f"DELETE FROM {table_name} WHERE category='{category}' AND item='{item}'")
+        connector = connection.cursor()
+        connector.execute(f"SELECT category, item from {table_name}")
+        response = connector.fetchall()
+        for c, i in response:
+            if i != item and c != item:
+                return f"Looks like there is no item or category with the name: {item}"
+        connection.execute(f"DELETE FROM {table_name} WHERE item='{item}' OR category='{item}'")
         connection.commit()
-        return f"Category: {category} and Item: {item} has been removed from {table_name}."
+        return f"Item: {item} has been removed from {table_name}."
 
 
 if __name__ == '__main__':
