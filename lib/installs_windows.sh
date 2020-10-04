ffb418b7647dca2ce5e6007d9e0d04e5b340e530
thevickypedia <vignesh.sivanandarao@gmail.com>
2020-10-04 08:54:45 -0500

    remove unnecessary installations

--- lib/installs_windows.sh
+++ lib/installs_windows.sh
@@ -1,13 +1,7 @@
 echo "**********************************************************************************************************"
-echo "Install Anaconda and make sure 'pip3' command works before proceeding."
-echo "Refer this link for pip3 installation guide: https://stackoverflow.com/questions/41501636/how-to-install-pip3-on-windows"
+echo "Make sure 'pip3' command works before proceeding."
+echo "Refer this link for pip3 setup guide: https://stackoverflow.com/questions/41501636/how-to-install-pip3-on-windows"
 echo "**********************************************************************************************************"
-echo "Starting conda installations"
-conda install portaudio
-conda install PyAudio
-echo "Starting pip3 installations"
-pip3 install numpy
-pip3 install pyttsx3
 pip3 install pipwin
 pipwin install pyaudio
 pip3 install -U pyttsx3==2.71
