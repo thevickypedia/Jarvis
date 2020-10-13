fef82ec71152363d5af49a13e41090f750bcb32e
thevickypedia <vignesh.sivanandarao@gmail.com>
2020-10-13 10:59:38 -0500

    pick random alarm tones handle file not found exception

--- alarm.py
+++ alarm.py
@@ -1,21 +1,32 @@
+import os
+import random
 import subprocess
-from threading import Thread
 import time
+from threading import Thread
 
 
 class Alarm(Thread):
     def __init__(self, hours, minutes):
         super(Alarm, self).__init__()
-        self.hours = int(hours)
-        self.minutes = int(minutes)
-        self.alarm_state = True
+        if hours and minutes:
+            self.hours = int(hours)
+            self.minutes = int(minutes)
+            self.alarm_state = True
 
     def run(self):
-        while self.alarm_state:
-            now = time.localtime()
-            if now.tm_hour == self.hours and now.tm_min == self.minutes:
-                subprocess.call(["open", "Early Riser.mp3"])
-                return
+        try:
+            directory = "mp3"
+            tone = random.choice(os.listdir(directory))
+            while self.alarm_state:
+                now = time.localtime()
+                if now.tm_hour == self.hours and now.tm_min == self.minutes:
+                    subprocess.call(["open", f"{directory}/{tone}"])
+                    return
+        except FileNotFoundError:
+            return
+
+    def kill_alarm(self):
+        self.alarm_state = False
 
 
 if __name__ == '__main__':
