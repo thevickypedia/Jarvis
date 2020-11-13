c4e54ca09e0911bd891536e03941eae437b0df1e
thevickypedia <vignesh.sivanandarao@gmail.com>
2020-11-13 12:03:11 -0600

    update version specific requirements

--- lib/installs_windows.sh
+++ lib/installs_windows.sh
@@ -3,30 +3,29 @@ echo "Make sure 'pip3' command works before proceeding."
 echo "Refer this link for pip3 setup guide: https://stackoverflow.com/questions/41501636/how-to-install-pip3-on-windows"
 echo "**********************************************************************************************************"
 pip3 install pipwin
-pipwin install pyaudio
+pipwin install pyaudio==0.2.11
 pip3 install -U pyttsx3==2.71
-pip3 install SpeechRecognition
-pip3 install pytemperature
-pip3 install psutil
-pip3 install wikipedia
-pip3 install newsapi-python
-pip3 install pyrh
-pip3 install pytz
-pip3 install pyicloud
-pip3 install geopy
-pip3 install PyDictionary
-pip3 install haversine
-pip3 install googlehomepush
-pip3 install axju-jokes
-pip3 install timezonefinder
-pip3 install inflect
-pip3 install wordninja
-pip3 install punctuator
-pip3 install search_engine_parser
-pip3 install boto3
-pip3 install playsound
-pip3 install pywebostv
-pip3 install wakeonlan
+pip3 install SpeechRecognition==3.8.1
+pip3 install pytemperature==1.0
+pip3 install psutil==5.7.2
+pip3 install wikipedia==1.4.0
+pip3 install newsapi-python==0.2.6
+pip3 install pyrh==2.0
+pip3 install pytz==2020.1
+pip3 install pyicloud==0.9.7
+pip3 install geopy==2.0.0
+pip3 install PyDictionary==2.0.1
+pip3 install googlehomepush==0.1.0
+pip3 install axju-jokes==1.0.3
+pip3 install timezonefinder==4.4.1
+pip3 install inflect==4.1.0
+pip3 install wordninja==2.0.0
+pip3 install punctuator==0.9.6
+pip3 install search_engine_parser==0.6.2
+pip3 install boto3==1.16.12
+pip3 install playsound==1.2.2
+pip3 install pywebostv==0.8.4
+pip3 install wakeonlan==1.1.6
 #pip3 install ChatterBot==1.0.0 --ignore-installed PyYAML
 #pip3 install chatterbot-corpus==1.2.0 --ignore-installed PyYAML
 #pip3 install sqlalchemy==1.2
