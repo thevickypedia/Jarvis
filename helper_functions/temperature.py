d049cfdbc25faa6933f53161e8e42821f0428b63
thevickypedia <vignesh.sivanandarao@gmail.com>
2021-03-30 17:32:14 -0500

    add doc strings and remove unused temperature conversions

--- helper_functions/temperature.py
+++ helper_functions/temperature.py
@@ -1,34 +1,28 @@
 def c2f(arg):
+    """Converts Celcius to Farenheit"""
     return round(((arg / 5) * 9) + 32, 2)
 
 
 def f2c(arg):
+    """Converts Farenheit to Celcius"""
     return round(((arg - 32) / 9) * 5, 2)
 
 
 def c2k(arg):
+    """Converts Celcius to Kelvin"""
     return arg + 273.15
 
 
 def k2c(arg):
+    """Converts Kelvin to Celcius"""
     return arg - 273.15
 
 
 def k2f(arg):
+    """Converts Kelvin to Celcius"""
     return round((arg * 1.8) - 459.69, 2)
 
 
 def f2k(arg):
+    """Converts Farenheit to Kelvin"""
     return round((arg + 459.67) / 1.8, 2)
-
-
-def r2c(arg):
-    return arg * 1.25
-
-
-def c2r(arg):
-    return round(arg * 0.8, 2)
-
-
-def r2k(arg):
-    return c2k(r2c(arg))
